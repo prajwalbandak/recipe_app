bea853517185514e8efeee4150e88a7733634baf
prajwalbandak <62823252+prajwalbandak@users.noreply.github.com>
2023-04-12 16:12:16 +0530

    Update Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,9 +5,7 @@ const Recipe = ({title,calories,image,ingredients}) => {
     <div>
         <h1>{title}</h1>
         <ol>
-            {ingredients.map(ingredient => (
-                <li>{ingredient.text}</li>
-            ))}
+            {ingredients.map(ingredient => (<li>{ingredient.text}</li>))}
         </ol>
         <p>{calories}</p>
         <img src={image} alt=""></img>
